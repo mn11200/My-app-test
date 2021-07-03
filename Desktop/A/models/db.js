a6e963359c763007395870f8539cfa78c2b572d6
Viet Nguyen <vietnqgcc18157@fpt.edu.vn>
2021-07-03 20:55:37 +0700

    first commit

---
+++ Desktop/A/models/db.js
@@ -0,0 +1,11 @@
+
+const mongoose = require('mongoose');
+const url = "mongodb+srv://mn300300:112233aA@cluster0.90r92.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongoose.connect(url,{useNewUrlParser:true},(err) => {
+    if(!err){ console.log("MongoDB Connection Succeeded");}
+    else{
+        console.log("An Error Occured");
+    } 
+})
+
+require('./product.model');
